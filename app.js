71bc6c97cde96abadc068d5645dce53e73d774b5
rohit1243r <rohit181975@gmail.com>
2026-02-02 10:36:07 +0530

    mini express app

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+let students = [
+    {id: 1, name: "Rohit", course: "CSE"},
+    {id: 2, name: "Amit", course: "IT"}
+];
+
+app.get('/', (req, res) => {
+    res.send("Welcome to root page API");
+});
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+
+app.post("/students", (req, res) => {
+    const newStudent = {
+        id: students.length + 1,
+        name: req.body.name,
+        course: req.body.course
+    }
+
+    students.push(newStudent);
+    res.status(201).json(newStudent);
+})
+app.listen(3000, () => {
+    console.log("Server running on port 8080");
+});
